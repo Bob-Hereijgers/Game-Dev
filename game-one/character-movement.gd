7309ce32fd8d50b8743c4f2d2b78a2644f6d2eee
Bob-hereijgers <bob.hereijgers@gmail.com>
2025-08-19 17:58:36 +0200

    zoom zoom crash

--- game-one/character-movement.gd
+++ game-one/character-movement.gd
@@ -1,12 +1,15 @@
 extends CharacterBody2D
 
-@export var speed = 4000
+
+@export var speed = 500
 
 func get_input():
 	var input_direction = Input.get_vector("move left", "move right", "move up", "move down")
 	velocity = input_direction * speed
 
-func _physics_process(delta):
-	print(Vector2.ZERO)
+func _physics_process(_delta):
 	get_input()
 	move_and_slide()
+	var viewport_rect = get_viewport_rect()
+	global_position.x = clamp(global_position.x, 0, viewport_rect.size.x)
+	global_position.y = clamp(global_position.y, 0, viewport_rect.size.y)
